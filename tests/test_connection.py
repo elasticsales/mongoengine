ca8cb222d99c3a5eedb1506dadd23dcb13589803
Stefan Wojcik <wojcikstefan@gmail.com>
2019-05-31 12:08:52 +0200

    Fix the test that asserts setting tz_aware on the connection
    Previously you could access the property via connection.tz_aware. In PyMongo
    v3.x this same property lives in connection.codec_options.tz_aware.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -74,12 +74,11 @@ class ConnectionTest(unittest.TestCase):
         """
         connect('mongoenginetest', alias='t1', tz_aware=True)
         conn = get_connection('t1')
-
-        self.assertTrue(conn.tz_aware)
+        self.assertTrue(conn.codec_options.tz_aware)
 
         connect('mongoenginetest2', alias='t2')
         conn = get_connection('t2')
-        self.assertFalse(conn.tz_aware)
+        self.assertFalse(conn.codec_options.tz_aware)
 
     def test_datetime(self):
         connect('mongoenginetest', tz_aware=True)
