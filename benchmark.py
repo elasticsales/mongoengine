ac8dd4d61e8daa69df540f19b5ca978714a33a9d
Stefan Wojcik <wojcikstefan@gmail.com>
2019-06-13 15:10:01 +0200

    Clean up the benchmark script
    This commit:
    1. Removes the cascade=save tests. It's not an option I'd recommend using AND
       it primarily matters if you have any reference fields in your document,
       which is not the case in this script.
    2. Uses PyMongo-v3.x-style write concern.
    3. Removes an old docstring describing some random benchmark run from the past.

--- benchmark.py
+++ benchmark.py
@@ -23,97 +23,6 @@ def cprofile_main():
 
 
 def main():
-    """
-    0.4 Performance Figures ...
-
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo
-    3.86744189262
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine
-    6.23374891281
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False
-    5.33027005196
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False, cascade=False
-    pass - No Cascade
-
-    0.5.X
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo
-    3.89597702026
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine
-    21.7735359669
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False
-    19.8670389652
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False, cascade=False
-    pass - No Cascade
-
-    0.6.X
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo
-    3.81559205055
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine
-    10.0446798801
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False
-    9.51354718208
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False, cascade=False
-    9.02567505836
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, force=True
-    8.44933390617
-
-    0.7.X
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo
-    3.78801012039
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine
-    9.73050498962
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False
-    8.33456707001
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, safe=False, validate=False, cascade=False
-    8.37778115273
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, force=True
-    8.36906409264
-    0.8.X
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo
-    3.69964408875
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - Pymongo write_concern={"w": 0}
-    3.5526599884
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine
-    7.00959801674
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries without continual assign - MongoEngine
-    5.60943293571
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine - write_concern={"w": 0}, cascade=True
-    6.715102911
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, write_concern={"w": 0}, validate=False, cascade=True
-    5.50644683838
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, write_concern={"w": 0}, validate=False
-    4.69851183891
-    ----------------------------------------------------------------------------------------------------
-    Creating 10000 dictionaries - MongoEngine, force_insert=True, write_concern={"w": 0}, validate=False
-    4.68946313858
-    ----------------------------------------------------------------------------------------------------
-    """
-
     setup = """
 from pymongo import MongoClient
 connection = MongoClient()
@@ -132,39 +41,39 @@ for i in xrange(10000):
     for j in range(20):
         example['fields']["key"+str(j)] = "value "+str(j)
 
-    noddy.save(example)
+    noddy.insert_one(example)
 
 myNoddys = noddy.find()
-[n for n in myNoddys] # iterate
+[n for n in myNoddys]  # iterate
 """
 
     print "-" * 100
-    print """Creating 10000 dictionaries - Pymongo"""
+    print """Creating 10000 dictionaries - PyMongo"""
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     stmt = """
-from pymongo import MongoClient
+from pymongo import MongoClient, WriteConcern
 connection = MongoClient()
 
 db = connection.timeit_test
-noddy = db.noddy
+noddy = db.noddy.with_options(write_concern=WriteConcern(w=0))
 
 for i in xrange(10000):
     example = {'fields': {}}
     for j in range(20):
         example['fields']["key"+str(j)] = "value "+str(j)
 
-    noddy.save(example, write_concern={"w": 0})
+    noddy.insert_one(example)
 
 myNoddys = noddy.find()
-[n for n in myNoddys] # iterate
+[n for n in myNoddys]  # iterate
 """
 
     print "-" * 100
-    print """Creating 10000 dictionaries - Pymongo write_concern={"w": 0}"""
+    print """Creating 10000 dictionaries - PyMongo write_concern={"w": 0}"""
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     setup = """
 from pymongo import MongoClient
@@ -187,13 +96,13 @@ for i in xrange(10000):
     noddy.save()
 
 myNoddys = Noddy.objects()
-[n for n in myNoddys] # iterate
+[n for n in myNoddys]  # iterate
 """
 
     print "-" * 100
-    print """Creating 10000 dictionaries - MongoEngine"""
+    print "Creating 10000 dictionaries - MongoEngine"
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     stmt = """
 for i in xrange(10000):
@@ -205,52 +114,36 @@ for i in xrange(10000):
     noddy.save()
 
 myNoddys = Noddy.objects()
-[n for n in myNoddys] # iterate
+[n for n in myNoddys]  # iterate
 """
 
     print "-" * 100
-    print """Creating 10000 dictionaries without continual assign - MongoEngine"""
+    print "Creating 10000 dictionaries without continual assign - MongoEngine"
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     stmt = """
 for i in xrange(10000):
     noddy = Noddy()
     for j in range(20):
         noddy.fields["key"+str(j)] = "value "+str(j)
-    noddy.save(write_concern={"w": 0}, cascade=True)
+    noddy.save(write_concern={"w": 0})
 
 myNoddys = Noddy.objects()
 [n for n in myNoddys] # iterate
 """
 
     print "-" * 100
-    print """Creating 10000 dictionaries - MongoEngine - write_concern={"w": 0}, cascade = True"""
+    print """Creating 10000 dictionaries - MongoEngine - write_concern={"w": 0}"""
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     stmt = """
 for i in xrange(10000):
     noddy = Noddy()
     for j in range(20):
         noddy.fields["key"+str(j)] = "value "+str(j)
-    noddy.save(write_concern={"w": 0}, validate=False, cascade=True)
-
-myNoddys = Noddy.objects()
-[n for n in myNoddys] # iterate
-"""
-
-    print "-" * 100
-    print """Creating 10000 dictionaries - MongoEngine, write_concern={"w": 0}, validate=False, cascade=True"""
-    t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
-
-    stmt = """
-for i in xrange(10000):
-    noddy = Noddy()
-    for j in range(20):
-        noddy.fields["key"+str(j)] = "value "+str(j)
-    noddy.save(validate=False, write_concern={"w": 0})
+    noddy.save(write_concern={"w": 0}, validate=False)
 
 myNoddys = Noddy.objects()
 [n for n in myNoddys] # iterate
@@ -259,7 +152,7 @@ myNoddys = Noddy.objects()
     print "-" * 100
     print """Creating 10000 dictionaries - MongoEngine, write_concern={"w": 0}, validate=False"""
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
     stmt = """
 for i in xrange(10000):
@@ -275,7 +168,7 @@ myNoddys = Noddy.objects()
     print "-" * 100
     print """Creating 10000 dictionaries - MongoEngine, force_insert=True, write_concern={"w": 0}, validate=False"""
     t = timeit.Timer(stmt=stmt, setup=setup)
-    print t.timeit(1)
+    print '{}s'.format(t.timeit(1))
 
 
 if __name__ == "__main__":
